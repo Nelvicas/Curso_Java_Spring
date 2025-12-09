e6437384e8e60dcb44a21c9ca1f65a8fb5c79c2f
nelviCastellanos <nelvicas97@gmail.com>
2025-12-08 22:28:33 -0600

    ejercicio uso de lo que se vio en curos

---
+++ desafio/src/main/java/com/aluracursos/desafio/model/DatosLibros.java
@@ -0,0 +1,15 @@
+package com.aluracursos.desafio.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibros(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<DatosaAutor> autor,
+        @JsonAlias("languages") List<String> idiomas,
+        @JsonAlias("download_count") Double numeroDeDescargas
+) {
+}
---
+++ desafio/src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -0,0 +1,37 @@
+package com.aluracursos.desafio.principal;
+
+import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
+import com.aluracursos.desafio.service.ConsumoAPI;
+import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
+
+
+public class Principal {
+
+    // atributos
+    private static final  String URL_BASE = "https://gutendex.com/books/";
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    // metodos
+    public void muestraElMenu(){
+        var json = consumoAPI.obtenerDatos(URL_BASE);
+        System.out.println(json);
+        var datos = conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+
+
+        ///  Top 10 libros mas descargados
+
+        System.out.println("Top 10 libros mas descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
+
+    }
+
+}
