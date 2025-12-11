f71a0dc559fe07bc2140cfb2c22d7346bb48dfa0
nelviCastellanos <nelvicas97@gmail.com>
2025-12-10 23:18:50 -0600

    uso de estructuras de datos (listas, set, map)

--- Colecciones_Streams/src/Ejercicio.java
+++ Colecciones_Streams/src/Ejercicio.java
@@ -12,8 +12,35 @@ public class Ejercicio {
         empleados.add("Carlos");
         empleados.add("Ana");
 
-        System.out.println(empleados
-        );
+        System.out.println(empleados);
+
+        ///   Eliminano elementos
+        System.out.println("///////  Eliminando elementos   /////");
+
+        List<String> inicial = new ArrayList<>();
+        inicial.add("Juana");
+        inicial.add("Lucas");
+        inicial.add("Pedro");
+        inicial.add("Antonio");
+
+        System.out.println(inicial);
+        System.out.println(inicial.remove("Pedro"));
+        System.out.println(inicial);
+
+
+
+        ///   Accediendo a elementos
+        System.out.println("///////  Accediendo a elementos   /////");
+
+        List<String> empleados2 = new ArrayList<>();
+        empleados2.add("Juan");
+        empleados2.add("María");
+        empleados2.add("Ana");
+        empleados2.add("Pedro");
+        empleados2.add("Antonio");
+
+        System.out.println("La segunda persona de la lista es: "+ empleados2.get(1));
+        System.out.println("Total de empleados: " + empleados2.toArray().length);
 
     }
 }
