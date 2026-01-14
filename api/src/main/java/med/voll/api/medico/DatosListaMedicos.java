1e50c5d24395cd87ea4393847331a48a9590ef1f
nelvi <nelvicas97@gmail.com>
2026-01-13 23:07:51 -0600

    desafio creacion d api rest(creasion de metodo get) obtener los datos de la d

---
+++ api/src/main/java/med/voll/api/medico/DatosListaMedicos.java
@@ -0,0 +1,18 @@
+package med.voll.api.medico;
+
+public record DatosListaMedicos(
+        String nombre,
+        String email,
+        String documento,
+        Especialidad especialidad
+    ) {
+
+    public DatosListaMedicos(Medico medico) {
+        this(
+                medico.getNombre(),
+                medico.getEmail(),
+                medico.getDocumento(),
+                medico.getEspecialidad()
+        );
+    }
+}
