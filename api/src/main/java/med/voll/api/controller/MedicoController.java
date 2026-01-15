29c4d654b74f73a94800e090b5f4a0db119bbac1
nelvi <nelvicas97@gmail.com>
2026-01-14 22:35:18 -0600

    desafio creacion d api rest(creasion de metodo get) paginacion

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +30,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListaMedicos> listar(){
-        return repository.findAll().stream().map(DatosListaMedicos::new).toList();
+    public Page<DatosListaMedicos> listar(Pageable paginacion){
+        return repository.findAll(paginacion).map(DatosListaMedicos::new);
     }
 }
